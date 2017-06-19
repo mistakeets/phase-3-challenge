51f6fa47ee44b91b7370c157a6df7d5477fe462a
Keith Oppel <mistakeets@gmail.com>
2017-06-19 10:28:03 -0700

    finishes implementation of zero route

--- part-1/server.js
+++ part-1/server.js
@@ -6,6 +6,10 @@ app.get('/', (request, response) => {
   response.send("Hello World")
 })
 
+app.get('/zero', (request, response) => {
+  response.send("0")
+})
+
 app.listen(3000, () => {
   console.log('Express Server started on port 3000')
 })
