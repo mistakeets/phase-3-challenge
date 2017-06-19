5dd90ff24da8a70605db5a120e7ff59eb448ae9f
Keith Oppel <mistakeets@gmail.com>
2017-06-19 16:38:02 -0700

    update schema to include orders. finishes schema

--- part-2/schema.sql
+++ part-2/schema.sql
@@ -12,5 +12,16 @@ CREATE TABLE item (
 
 CREATE TABLE shopper (
   shopper_id SERIAL PRIMARY KEY,
-  shopper_name VARCHAR(50)
+  shopper_name VARCHAR(50),
+  phone VARCHAR(25),
+  street_address VARCHAR(100),
+  state VARCHAR(2),
+  zipcode INTEGER
+);
+
+CREATE TABLE grocery_order (
+  order_id SERIAL PRIMARY KEY,
+  order_time TIMESTAMP,
+  shopper_id SERIAL REFERENCES shopper (shopper_id),
+  item_id SERIAL REFERENCES item (item_id)
 );
