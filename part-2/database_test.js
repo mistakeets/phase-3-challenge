db2442465588149c867c3456024638ca2458af58
Keith Oppel <mistakeets@gmail.com>
2017-06-20 10:44:20 -0700

    finishes database tests

---
+++ part-2/database_test.js
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+const { itemsInSection, cheapItems, countItemsInSection } = require('./database.js')
+
+it('items in section bulk returns Flour Pasta and Rice', finish => {
+  itemsInSection('bulk').then(answers => {
+    const things = answers.map(answer => answer.thing)
+    expect(things).to.include('Flour')
+    expect(things).to.include('Pasta')
+    expect(things).to.include('Rice')
+    finish()
+  })
+})
+
+it('cheapItems returns Fish as the first item and Honey as the last item', finish => {
+  cheapItems().then(answers => {
+    expect(answers[0].price).to.equal('$0.49')
+    expect(answers[answers.length - 1].price).to.equal('$9.31')
+    finish()
+  })
+})
+
+it('countItemsInSection packaged returns 5', finish => {
+  countItemsInSection('packaged').then(answer => {
+    expect(answer).to.equal('5')
+    finish()
+  })
+})
