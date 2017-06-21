66481d8279937a2dee51ddf8ebb4d553493181fb
Keith Oppel <mistakeets@gmail.com>
2017-06-21 02:39:25 -0700

    refactor itemSection and cheapItems tests

--- part-2/database_test.js
+++ part-2/database_test.js
@@ -1,40 +1,52 @@
 const chai = require('chai')
 const expect = chai.expect
-const { itemsInSection, cheapItems, countItemsInSection } = require('./database.js')
+const dbQuery = require('./database')
 
-it('items in section bulk returns Flour Pasta and Rice', done => {
-  itemsInSection('bulk').then(answers => {
-      const things = answers.map(answer => answer.thing)
-      expect(things).to.include('Flour')
-      expect(things).to.include('Pasta')
-      expect(things).to.include('Rice')
-      done()
-    })
-    .catch(err => {
-      console.log(err)
-      done()
-    })
+describe('itemsInSection(bulk)', () => {
+  it('returns items Flour Pasta Rice', done => {
+    dbQuery.itemsInSection('bulk')
+      .then(item => {
+        expect(item[0].name).to.equal('Flour')
+        expect(item[1].name).to.equal('Pasta')
+        expect(item[2].name).to.equal('Rice')
+        done()
+      })
+      .catch(err => {
+        console.log(err)
+      })
+    done()
+  })
 })
 
-it('cheapItems returns Fish as the first item and Honey as the last item', done => {
-  cheapItems().then(answers => {
-      expect(answers[0].price).to.equal('$0.49')
-      expect(answers[answers.length - 1].price).to.equal('$9.31')
-      done()
-    })
-    .catch(err => {
-      console.log(err)
-      done()
-    })
+describe('cheapItems()', () => {
+  it('returns the item Fish as first item and Honey as last item', done => {
+    dbQuery.cheapItems()
+      .then(item => {
+        expect(item[0].name).to.equal('Fish')
+        expect(item[item.length - 1].name).to.equal('Honey')
+        done()
+      })
+      .catch(console.log)
+    done()
+  })
 })
 
-it('countItemsInSection packaged returns 5', done => {
-  countItemsInSection('packaged').then(answer => {
-      expect(answer).to.equal('5')
-      done()
-    })
-    .catch(err => {
-      console.log(err)
-      done()
-    })
-})
+// it('cheapItems returns Fish as the first item and Honey as the last item', done => {
+//   cheapItems().then(answers => {
+//       expect(answers[0].price).to.equal('$0.49')
+//       expect(answers[answers.length - 1].price).to.equal('$9.31')
+//       done()
+//     })
+//     .catch(console.log)
+// })
+// })
+
+// it('countItemsInSection packaged returns 5', done => {
+//   countItemsInSection('packaged').then(answer => {
+//       expect(answer).to.equal('5')
+//       done()
+//     })
+//     .catch(err => {
+//       console.log(err)
+//     })
+// })
